5528ddd6bf72487db6b08e71808610cb3b962233
Lexi Morgan <leximorgan816@gmail.com>
2019-02-04 16:18:47 -0600

    added page view counter and can pull the name from the query

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -7,10 +7,24 @@ import java.io.PrintWriter;
 
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+    int counter = 0;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        counter+=1;
+        String queryString = request.getParameter("name");
+        if(queryString != null) {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<h1>Hello, " + queryString + "!</h1>");
+        } else {
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<h1>Hello, World!"+ "</h1>");
+        }
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
+        out.println("<h5>Total page Views: " + counter + "</h5>");
+
+
     }
 }
